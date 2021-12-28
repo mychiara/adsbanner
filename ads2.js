4d0fb85ce2af98160688ad6d2fe2deb749143b94
mychiara <85479169+mychiara@users.noreply.github.com>
2021-12-28 23:25:45 +0800

    Update ads2.js

--- ads2.js
+++ ads2.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : 'c5ba7a3b515b1eb51835f2066ed18bdd',
+		'key' : '7c694f554868ee4bf34d1a35589b35b4',
 		'format' : 'iframe',
 		'height' : 600,
 		'width' : 160,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/c5ba7a3b515b1eb51835f2066ed18bdd/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/7c694f554868ee4bf34d1a35589b35b4/invoke.js"></scr' + 'ipt>');
