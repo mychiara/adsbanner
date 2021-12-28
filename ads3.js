582aca07250dde086a6cdbad910c4f9687daf4f4
mychiara <85479169+mychiara@users.noreply.github.com>
2021-12-28 23:47:18 +0800

    Add files via upload

---
+++ ads3.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '98997cebcc422dcf318d719442cb1a7d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/98997cebcc422dcf318d719442cb1a7d/invoke.js"></scr' + 'ipt>');
