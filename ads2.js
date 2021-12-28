098efc700f6499e12453f34fd256c8cee9a36ab8
mychiara <85479169+mychiara@users.noreply.github.com>
2021-12-28 23:10:24 +0800

    Add files via upload

---
+++ ads2.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c5ba7a3b515b1eb51835f2066ed18bdd',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/c5ba7a3b515b1eb51835f2066ed18bdd/invoke.js"></scr' + 'ipt>');
