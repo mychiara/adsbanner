0f1b6e878c7577c0c55e55cf6d74341e88cd859a
mychiara <85479169+mychiara@users.noreply.github.com>
2021-12-28 23:27:38 +0800

    Update ads2.js

--- ads2.js
+++ ads2.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '7c694f554868ee4bf34d1a35589b35b4',
+		'key' : '4c9aa47ae7ef6668fa8e8c118d9a815a',
 		'format' : 'iframe',
 		'height' : 600,
 		'width' : 160,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/7c694f554868ee4bf34d1a35589b35b4/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://racingorchestra.com/4c9aa47ae7ef6668fa8e8c118d9a815a/invoke.js"></scr' + 'ipt>');
