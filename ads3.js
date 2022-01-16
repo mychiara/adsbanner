c3fddafad52b5bd11e339deec20ba4b1c4116b42
mychiara <85479169+mychiara@users.noreply.github.com>
2022-01-16 22:05:11 +0800

    Update ads3.js

--- ads3.js
+++ ads3.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '98997cebcc422dcf318d719442cb1a7d',
+		'key' : 'd04b5e8712960b5564b30644da7c67ea',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 90,
+		'width' : 728,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nearestaxe.com/98997cebcc422dcf318d719442cb1a7d/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/d04b5e8712960b5564b30644da7c67ea/invoke.js"></scr' + 'ipt>');
