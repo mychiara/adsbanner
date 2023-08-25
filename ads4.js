25d387bedf11c469a2f558658ab9c2d7559697c9
mychiara <85479169+mychiara@users.noreply.github.com>
2023-08-25 08:40:58 +0800

    Update ads4.js

--- ads4.js
+++ ads4.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : '887d1ebe7501f6544975e832f7527848',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/887d1ebe7501f6544975e832f7527848/invoke.js"></scr' + 'ipt>');
+
