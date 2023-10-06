ef9d72d213bccfadac20df9dce1948449d62c10b
mychiara <85479169+mychiara@users.noreply.github.com>
2023-10-06 22:25:43 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,8 +1,8 @@
-atOptions = {
-		'key' : '887d1ebe7501f6544975e832f7527848',
+	atOptions = {
+		'key' : 'd04b5e8712960b5564b30644da7c67ea',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 90,
+		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/887d1ebe7501f6544975e832f7527848/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/d04b5e8712960b5564b30644da7c67ea/invoke.js"></scr' + 'ipt>');
